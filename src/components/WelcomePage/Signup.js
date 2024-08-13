049742228157ef7de3c1c5483b7da23a9d30abd9
yaelaflalo <aflaloyael@gmail.com>
2024-08-13 16:19:06 +0300

    fix bug

--- src/components/WelcomePage/Signup.js
+++ src/components/WelcomePage/Signup.js
@@ -67,7 +67,7 @@ const Signup = () => {
 
     try {
       const response = await signup(nameValue, emailValue, passwordValue);
-      loginHandler(response.user.name, response.user.role, response.user.email);
+      loginHandler(response.user.name, response.user.email, response.user.role);
       navigate("/me");
     } catch (error) {
       console.log(error);
