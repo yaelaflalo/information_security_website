8244ce86b2ee2979fa4702fe977c517fff82bee1
yaelaflalo <aflaloyael@gmail.com>
2024-08-13 07:55:16 +0300

    top navbar

---
+++ src/components/Layout/MainHeader.js
@@ -0,0 +1,96 @@
+import { NavLink } from "react-router-dom";
+import classes from "./MainHeader.module.css";
+import DropList from "./DropList";
+import { useState, useContext } from "react";
+import { useNavigate } from "react-router-dom";
+
+import logo from "../../assets/logo.png";
+import { logout, logoutAll } from "../../Services/auth";
+import UserContext from "../../context/user-context";
+
+const MainHeader = () => {
+  const [openRequestsOptions, setOpenRequestsOptions] = useState(false);
+  const { role } = useContext(UserContext);
+
+  const openDropdownHandler = () => {
+    setOpenRequestsOptions(!openRequestsOptions);
+  };
+
+  const closeDropdownHandler = () => {
+    setOpenRequestsOptions(false);
+  };
+
+  const navigate = useNavigate();
+
+  const logoutHandler = async () => {
+    try {
+      await logout();
+    } catch {
+      console.error("Enable to logout");
+    }
+  };
+
+  const logoutallHandler = async () => {
+    try {
+      await logoutAll();
+    } catch {
+      console.error("Enable to logout all");
+    }
+  };
+
+  return (
+    <header className={classes.header}>
+      <nav>
+        <ul>
+          <li>
+            <NavLink
+              className={({ isActive }) => (isActive ? classes.active : "")}
+              to="/welcome"
+              onClick={logoutallHandler}
+            >
+              התנתקות מכל המכשירים
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              className={({ isActive }) => (isActive ? classes.active : "")}
+              to="/welcome"
+              onClick={logoutHandler}
+            >
+              התנתקות
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to={navigate.path} onClick={openDropdownHandler}>
+              בקשות ב"מ
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              className={({ isActive }) => (isActive ? classes.active : "")}
+              to="/me"
+              onClick={closeDropdownHandler}
+            >
+              אזור אישי
+            </NavLink>
+          </li>
+          {role === "admin" && (
+            <li>
+              <NavLink
+                className={({ isActive }) => (isActive ? classes.active : "")}
+                to="/admin"
+                onClick={closeDropdownHandler}
+              >
+                ניהול בקשות
+              </NavLink>
+            </li>
+          )}
+        </ul>
+      </nav>
+      {openRequestsOptions && <DropList closeDropDown={closeDropdownHandler} />}
+      <img src={logo} alt="website logo" className={classes.logo} />
+    </header>
+  );
+};
+
+export default MainHeader;
