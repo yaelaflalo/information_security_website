99fb54ad4a48fbe85765d9df7ffd7cf47783de26
yaelaflalo <aflaloyael@gmail.com>
2024-08-13 13:46:38 +0300

    change description

--- src/components/ProtectedRoute/UnauthorizedPage.js
+++ src/components/ProtectedRoute/UnauthorizedPage.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const UnauthorizedPage = () => {
-  return <h1>Access Denied: You do not have permission to view this page.</h1>;
+  return <h1>אין לך הרשאות לבקר בעמוד זה</h1>;
 };
 
 export default UnauthorizedPage;
