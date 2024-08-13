42b4a2ce221bfa3953be4ee884c608f6a5119a34
yaelaflalo <aflaloyael@gmail.com>
2024-08-13 08:00:00 +0300

    page for users without access to pages with permissions

---
+++ src/components/ProtectedRoute/UnauthorizedPage.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const UnauthorizedPage = () => {
+  return <h1>Access Denied: You do not have permission to view this page.</h1>;
+};
+
+export default UnauthorizedPage;
