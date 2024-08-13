f3c3c397e2b06aa6f0976e5edb707f3025cb3309
yaelaflalo <aflaloyael@gmail.com>
2024-08-13 09:22:34 +0300

    add date filter

--- src/components/ManagePlace/FilterButtons.js
+++ src/components/ManagePlace/FilterButtons.js
@@ -1,9 +1,30 @@
-import React from "react";
 import classes from "./FilterButtons.module.css";
+import Select from "react-select";
+import { useState } from "react";
 
-const FilterButtons = ({ onHistoryClick, onWaitingListClick }) => {
+const filteringDate = [
+    { value: "createdAt:desc", label: "מהחדש לישן" },
+    { value: "createdAt:asc", label: "מהישן לחדש" },
+  ];
+
+const FilterButtons = ({ onHistoryClick, onWaitingListClick, onDateChange }) => {
+    const [selectedFilteringDate, setSelectedFilteringDate] =
+    useState({value: "desc", label: "מהחדש לישן" });
+
+    const handleDateChange = (selectedOption) => {
+        setSelectedFilteringDate(selectedOption);
+        onDateChange(selectedOption);
+      }
+
   return (
     <div className={classes["filter-buttons"]}>
+        <Select
+        defaultValue={selectedFilteringDate}
+        onChange={handleDateChange}
+        options={filteringDate}
+        isSearchable
+        className={classes["filtering-date-selector"]}
+      />
       <button onClick={onHistoryClick} className={classes.button}>
         היסטורית בקשות
       </button>
