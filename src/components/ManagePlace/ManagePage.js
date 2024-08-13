7a3d9905343268cbe9667fb6dc5964457d818976
yaelaflalo <aflaloyael@gmail.com>
2024-08-13 07:57:02 +0300

    admin additional page

---
+++ src/components/ManagePlace/ManagePage.js
@@ -0,0 +1,96 @@
+import MainHeader from "../Layout/MainHeader";
+import classes from "./ManagePage.module.css";
+
+import { getAllRequests } from "../../Services/requests";
+import { useEffect, useState, useContext } from "react";
+import UnapproveDialog from "./UnapproveDialog";
+import { setRequestToUnapprove } from "../../Services/requests";
+import UserContext from "../../context/user-context";
+import BottomNavigation from "../Requests/BottomNavigation";
+import FilterButtons from "./FilterButtons";
+import RequestsList from "./RequestsList";
+
+const ManagePage = () => {
+  const [usersRequests, setUsersRequests] = useState([]);
+  const [skip, setSkip] = useState(0);
+  const limit = 5;
+  const [refreshRequests, setRefreshRequests] = useState(false);
+  const [rejectedRequestId, setRejectedRequestId] = useState("");
+  const [typeOfStatus, setTypeOfStatus] = useState("ממתין לאישור...");
+  const { userName } = useContext(UserContext);
+
+  const nextButtonHandler = () => {
+    setSkip((prevSkip) => prevSkip + limit);
+  };
+
+  const backButtonHandler = () => {
+    setSkip((prevSkip) => Math.max(prevSkip - limit, 0));
+  };
+
+  const handleRefresh = () => {
+    setRefreshRequests(!refreshRequests);
+  };
+
+  const handleRejectRequest = (requestId) => {
+    setRejectedRequestId(requestId);
+  };
+
+  const changeToHistory = () => {
+    setTypeOfStatus("");
+  };
+
+  const changeToWaitingList = () => {
+    setTypeOfStatus("ממתין לאישור...");
+  };
+
+  const closeUnapproveDialog = async (unapproveReasonValue) => {
+    try {
+      await setRequestToUnapprove(rejectedRequestId, {
+        unapproved_reason: `${unapproveReasonValue}`,
+      });
+    } catch (error) {
+      console.log(error.message);
+      return;
+    }
+    setRejectedRequestId("");
+    handleRefresh();
+  };
+
+  useEffect(() => {
+    const fetchRequests = async () => {
+      try {
+        const sortBy = "";
+
+        const data = await getAllRequests(typeOfStatus, sortBy, limit, skip);
+        setUsersRequests(data);
+      } catch (error) {
+        console.log("fetch failed");
+      }
+    };
+    fetchRequests();
+  }, [skip, refreshRequests, typeOfStatus]);
+
+  return (
+    <div>
+      <MainHeader />
+      <div className={classes["welcome-sign"]}>ברוך שובך {userName}!</div>
+      <div className={classes.center}>
+        <RequestsList
+          requests={usersRequests}
+          onRefresh={handleRefresh}
+          onReject={handleRejectRequest}
+        />
+      </div>
+      <BottomNavigation onNext={nextButtonHandler} onBack={backButtonHandler} />
+      {rejectedRequestId !== "" && (
+        <UnapproveDialog onSubmit={closeUnapproveDialog} />
+      )}
+      <FilterButtons
+        onHistoryClick={changeToHistory}
+        onWaitingListClick={changeToWaitingList}
+      />
+    </div>
+  );
+};
+
+export default ManagePage;
