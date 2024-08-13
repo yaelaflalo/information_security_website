efe610baac8ba811a41eecf6f351ab15c384ebca
yaelaflalo <aflaloyael@gmail.com>
2024-08-13 07:56:20 +0300

    request list in admin side

---
+++ src/components/ManagePlace/AvailableRequests.js
@@ -0,0 +1,56 @@
+import RequestItem from "../Requests/RequestItem/RequestItem";
+import ApproveButtons from "./ApproveButtons";
+import classes from "./AvailableRequests.module.css";
+
+import {
+  setRequestToApprove,
+} from "../../Services/requests";
+
+const AvailableRequests = ({
+  id,
+  requestType,
+  requestStatus,
+  requestDate,
+  requestReason,
+  unapprovedReason,
+  onRefresh,
+  onReject
+}) => {
+
+  const onApproveHandler = async () => {
+    try {
+      await setRequestToApprove(id);
+    } catch (error) {
+      console.log(error.message);
+    }
+    onRefresh();
+  };
+
+  const onRejectHandler = () => {
+    onReject(id);
+  };
+
+  return (
+      <li>
+        <div className={classes.container}>
+          <RequestItem
+            key={id}
+            requestType={requestType}
+            requestStatus={requestStatus}
+            requestDate={requestDate}
+            requestReason={requestReason}
+            unapprovedReason={unapprovedReason}
+          >
+            <ApproveButtons
+              onApproveHandler={onApproveHandler}
+              onRejectHandler={onRejectHandler}
+              onRefresh={onRefresh}
+            />
+          </RequestItem>
+        </div>
+      </li>
+  );
+};
+
+export default AvailableRequests;
+
