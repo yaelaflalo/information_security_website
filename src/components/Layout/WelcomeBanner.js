23c6c204a588ec34406ef139bac4619d13c3491d
yaelaflalo <aflaloyael@gmail.com>
2024-08-13 09:39:36 +0300

    spread the welcome sign to another component

---
+++ src/components/Layout/WelcomeBanner.js
@@ -0,0 +1,11 @@
+import classes from "./WelcomeBanner.module.css";
+import { useContext } from "react";
+import UserContext from "../../context/user-context";
+
+const WelcomeBanner = () => {
+    const { userName } = useContext(UserContext);
+
+  return <div className={classes["welcome-sign"]}>ברוך שובך {userName}!</div>;
+};
+
+export default WelcomeBanner;
